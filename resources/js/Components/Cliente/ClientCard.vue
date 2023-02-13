278b65feb7eb0ff0afd3f50bc138c5380b329d3b
JuanMira <juanmira720@gmail.com>
2023-02-13 13:21:01 -0500

    Componente card para renderizar la informacion de cada cliente

---
+++ resources/js/Components/Cliente/ClientCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["client"]);
+</script>
+<template>
+    <div
+        class="mx-auto bg-white shadow-sm rounded-lg max-w-5xl p-4 flex justify-between"
+    >
+        <div class="flex flex-col">
+            <span class="text-lg font-semibold text-gray-600">{{
+                client.nombre
+            }}</span>
+            <span class="text-sm font-medium text-gray-500">{{ client.email }}</span>
+        </div>
+    </div>
+</template>
